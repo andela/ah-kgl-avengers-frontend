3e5202f80fe30ce59e3fd03cf08abd2360e061f8
aaronsekisambu <aaron.sekisambu@gmail.com>
2019-06-05 11:16:16 +0200

    chore(routing): Setting up react-router-v5
    - use react-router v5 to access different routes
    
    [Delivers ##165020159]

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Route, Link, Switch,
+} from 'react-router-dom';
 import './index.css';
 
-const App = () => (
+// Temporary functional components will be deleted after project setup.
+const Home = () => (
   <div className="container">
-    <h1 className="app-title">Authors Haven</h1>
-    <p>Welcome!</p>
+    <h1 className="app-title">Home</h1>
+    <p>
+      <Link to="/sign-up">Sign up Now </Link>
+    </p>
   </div>
+);
+
+const About = () => (
+  <div className="container">
+    <h1 className="app-title" style={{ color: '#099EBF' }}>
+      About
+    </h1>
+    <p>
+      <Link to="/">home </Link>
+    </p>
+  </div>
+);
+
+const SignUp = () => (
+  <div className="container">
+    <h1 className="app-title" style={{ color: '#09BF86' }}>
+      Sign up
+    </h1>
+    <p>
+      <Link to="/about">About </Link>
+    </p>
+  </div>
+);
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/sign-up" component={SignUp} />
+    </Switch>
+  </Router>
 );
 
 export default App;
